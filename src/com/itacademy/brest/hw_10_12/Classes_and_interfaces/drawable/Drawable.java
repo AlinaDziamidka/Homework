132490624bcf950e6fe4116cf5f81aa15c88324f
Panda <al_1303@tut.by>
2023-10-26 19:58:26 +0300

    Commit_5

---
+++ src/com/itacademy/brest/hw_10_12/Classes_and_interfaces/drawable/Drawable.java
@@ -0,0 +1,12 @@
+package com.itacademy.brest.hw_10_12.Classes_and_interfaces.drawable;
+
+public interface Drawable {
+    void draw ();
+}
+
+
+
+
+
+//2) Определите интерфейс Drawable с помощью метода draw(). Создайте два класса, Circle и Rectangle,
+//  реализующие этот интерфейс. Каждый класс должен предоставлять собственную реализацию метода draw().
