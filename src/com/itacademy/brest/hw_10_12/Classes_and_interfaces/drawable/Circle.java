9cb28fa226ebaa312d617da9f6e1aa1096f968e0
Panda <al_1303@tut.by>
2023-10-26 19:58:26 +0300

    Commit_5

---
+++ src/com/itacademy/brest/hw_10_12/Classes_and_interfaces/drawable/Circle.java
@@ -0,0 +1,9 @@
+package com.itacademy.brest.hw_10_12.Classes_and_interfaces.drawable;
+
+public class Circle implements Drawable {
+
+    @Override
+    public void draw() {
+        System.out.println("Draw a circle");
+    }
+}
